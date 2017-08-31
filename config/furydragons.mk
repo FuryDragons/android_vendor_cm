420b4e379b654b1e88e0cb645d27086d410796ad
Nyks45 <winglow45@gmail.com>
2017-08-31 22:17:50 +0300

    Bootanim and Makefile Mods

---
+++ config/furydragons.mk
@@ -0,0 +1,11 @@
+#PLEASE INCLUDE ALL FuryDragons THINGS (APK,XML) HERE
+
+# Inherit common FuryDragons stuff
+
+# Required FuryDragons packages
++PRODUCT_PACKAGES += \
+FuryDragonsSetupWizard \
+FuryDragonsWallpapers
+
+#Required FuryDragons Files
++PRODUCT_COPY_FILES += \
